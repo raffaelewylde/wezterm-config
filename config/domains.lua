e0bdd5425d1c4d272bbbbde94a3fdeb854bcf211
Raffaele Wylde <75472685+raffaelewylde@users.noreply.github.com>
2024-11-21 06:07:20 -0500

    uncommented domains

--- config/domains.lua
+++ config/domains.lua
@@ -1,9 +1,9 @@
---return {
+return {
 -- ref: https://wezfurlong.org/wezterm/config/lua/SshDomain.html
---   ssh_domains = {},
+   ssh_domains = {},
 
 -- ref: https://wezfurlong.org/wezterm/multiplexing.html#unix-domains
---   unix_domains = {},
+   unix_domains = {},
 
 -- ref: https://wezfurlong.org/wezterm/config/lua/WslDomain.html
 ---   wsl_domains = {
@@ -15,4 +15,4 @@
 --         default_prog = { 'fish', '-l' },
 --      },
 --   },
---}
+}
